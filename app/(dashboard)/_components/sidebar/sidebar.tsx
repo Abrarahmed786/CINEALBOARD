fb3f812d206c9af237cfccfcaeef288166436a77
Abrar <arron@Louis.local>
2024-02-10 19:02:14 +0530

    added dashboard listing ui component

--- app/(dashboard)/_components/sidebar/sidebar.tsx
+++ app/(dashboard)/_components/sidebar/sidebar.tsx
@@ -1,7 +1,9 @@
+import { NewButton } from "./newButton"
+
 export const SideBar = () => {
     return (
         <aside className="fixed z-10 left-0 bg-teal-100 bg-blue-200 h-full w-[60px] flex p-3 flex-col gap-y-4">
-            Side Bar
+            <NewButton />
         </aside>
     )
 }
